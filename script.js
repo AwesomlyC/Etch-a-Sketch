e1f3779c9bacdf9984960575ae99620df6a7f825
AwesomlyC <109203183+AwesomlyC@users.noreply.github.com>
2024-03-02 17:38:54 -0800

    fixed the background color when hovering over a block

--- script.js
+++ script.js
@@ -13,9 +13,48 @@ const rows = document.querySelectorAll(".row");
 rows.forEach((row) => 
 {
     for (let i = 0; i < 16; i++){
-            const box = document.createElement("div");
-            box.textContent = "BOX";
-            row.appendChild(box);
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", function (){
+                square.style.backgroundColor="yellow";
+            });
+            row.appendChild(square);
         }
-        // console.log("HELP");
 });
+
+
+function changeColor(){
+
+}
+
+function generateBoard(){
+    let size = prompt("Please enter the size of board you want to generate between 0 - 100!");
+    while (size > 100 || size < 0){
+        size = prompt("Please enter a valid size");
+    }
+    // Clear board
+    const rows = document.querySelectorAll(".row");
+    rows.forEach((row) => {
+        row.remove();
+    });
+
+    // Create corresponding size board
+    const container = document.querySelector(".container");
+    let width = 960/size;
+
+    for (let i = 0; i < size; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < size; j++){
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", function (){
+                square.style.backgroundColor="yellow";
+            });
+            square.style.cssText = "width:" + width + ";";
+            row.appendChild(square);
+            
+        }
+        container.appendChild(row);
+    }
+};
